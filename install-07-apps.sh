dc2d704466db63bca9ac75b644ab10d7f93b18f6
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-04-27 05:27:09 +1000

    Added another app install script

---
+++ install-07-apps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+yaourt -S alsi aspell bc colordiff colorsvn cope-git dictd diction dmidecode dehtml elinks iftop languagetool lesspipe highlight mpd ncdu ncmpcpp feh htop hwinfo pydf ranger rsync rtorrent extract dzen2

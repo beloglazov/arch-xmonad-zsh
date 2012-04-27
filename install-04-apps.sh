578d7e038d47bca5eae9011e9ba7dfbf08cdd578
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-04-28 01:58:39 +1000

    Added chromium-libpdf

--- install-04-apps.sh
+++ install-04-apps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo pacman -S xorg xorg-xinit xmonad xmonad-contrib consolekit rxvt-unicode dmenu numlockx chromium rsync jdk7-openjdk apache-ant maven lftp unzip tree subversion
-yaourt urxvt-perls-git
+yaourt urxvt-perls-git chromium-stable-libpdf

6710baca929c5e40c8f87203ef8bec30d91495c9
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-05-04 18:01:10 +1000

    Added ranger preview dependecies

--- install-07-apps.sh
+++ install-07-apps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yaourt -S alsi aspell bc colordiff colorsvn cope-git dictd diction dmidecode dehtml elinks iftop languagetool lesspipe highlight mpd ncdu ncmpcpp feh htop hwinfo pydf ranger rsync rtorrent extract dzen2 emacs-bzr
+yaourt -S alsi aspell bc colordiff colorsvn cope-git dictd diction dmidecode dehtml elinks iftop languagetool lesspipe highlight mpd ncdu ncmpcpp feh htop hwinfo pydf ranger highlight atool libcaca rsync rtorrent extract dzen2 emacs-bzr

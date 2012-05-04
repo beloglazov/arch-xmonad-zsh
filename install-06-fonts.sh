c392f26a95ed9915a4c8bafda41726dbc4a9732b
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-05-04 10:06:46 +1000

    Fixed infinality setup

--- install-06-fonts.sh
+++ install-06-fonts.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 yaourt -S xorg-xfontsel terminus-font terminus-font-ttf ttf-dejavu otf-texgyre ttf-mac-fonts ttf-ms-fonts 
-yaourt -S freetype2-git-infinality
-#fontconfig-infinality
 #ttf-win7-fonts
+
+yaourt -S freetype2-infinality
+yaourt -S fontconfig-infinality
+yaourt -S lib32-freetype2-infinality

c3b5ad75768692ef29042e4c70aa2ced9ac84e72
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-05-04 10:13:13 +1000

    Added auto setting infinality to the win7 style

--- install-06-fonts.sh
+++ install-06-fonts.sh
@@ -6,3 +6,5 @@ yaourt -S xorg-xfontsel terminus-font terminus-font-ttf ttf-dejavu otf-texgyre t
 yaourt -S freetype2-infinality
 yaourt -S fontconfig-infinality
 yaourt -S lib32-freetype2-infinality
+
+sudo /etc/fonts/infinality/infctl.sh setstyle win7

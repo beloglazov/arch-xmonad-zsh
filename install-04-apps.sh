74ffe049b9c3e29a966d3912e4e398c1ca19b0d7
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-05-10 07:28:21 +1000

    Added openntpd

--- install-04-apps.sh
+++ install-04-apps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo pacman -S xorg xorg-xinit xmonad xmonad-contrib consolekit rxvt-unicode dmenu numlockx xxkb chromium rsync jdk7-openjdk apache-ant maven lftp unzip tree subversion slock alsa-utils
-yaourt urxvt-perls-git chromium-stable-libpdf flashplugin autojump
+yaourt urxvt-perls-git chromium-stable-libpdf flashplugin autojump openntpd
 
 ranger --copy-config=scope
 sudo ranger --copy-config=scope

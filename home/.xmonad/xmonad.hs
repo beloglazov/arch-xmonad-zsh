e5009455ff4b9ee8215511d55f4e1b39dbf83c8c
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-29 11:35:06 +1000

    xmonad: added key bindings for window shifting and changing the number of windows in the main area on M-S-arrows

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -9,9 +9,24 @@ import System.IO
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 
+myLayout = tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+
 main = do
 	xmonad $ ewmh defaultConfig
-		{ layoutHook = smartBorders $ layoutHook defaultConfig
+		{ layoutHook = smartBorders $ myLayout
 		, terminal		= "urxvtc"
 		, modMask		= mod4Mask
 		, borderWidth		= 2
@@ -26,9 +41,13 @@ main = do
                  , ((mod4Mask, xK_u), spawn "dmenu-urxvt")
 		 , ((mod4Mask, xK_p), spawn "dmenu-run")
                  , ((mod4Mask, xK_Right), windows W.focusDown)
-                 , ((mod4Mask, xK_Left),  windows W.focusUp  )
+                 , ((mod4Mask, xK_Left),  windows W.focusUp)
                  , ((mod4Mask, xK_Down),  sendMessage Shrink)
                  , ((mod4Mask, xK_Up),    sendMessage Expand)
+                 , ((mod4Mask .|. shiftMask, xK_Right), windows W.swapDown)
+                 , ((mod4Mask .|. shiftMask, xK_Left),  windows W.swapUp)
+                 , ((mod4Mask .|. shiftMask, xK_Down), sendMessage (IncMasterN (-1)))
+                 , ((mod4Mask .|. shiftMask, xK_Up),   sendMessage (IncMasterN 1))
 		 , ((mod4Mask .|. shiftMask, xK_F10), spawn "slock")
 		 , ((mod4Mask .|. shiftMask, xK_F11), spawn "reboot")
 		 , ((mod4Mask .|. shiftMask, xK_F12), spawn "shutdown")

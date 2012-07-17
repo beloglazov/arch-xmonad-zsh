bcc35528a6d97cb254e0282617e0db34ff0c7f9d
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-17 13:52:31 +1000

    Added w3m and emacs-w3m-cvs

--- install-08-apps.sh
+++ install-08-apps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yaourt -S alsi aspell bc colordiff colorsvn cope-git dictd diction dmidecode dehtml elinks iftop lesspipe highlight mpd ncdu ncmpcpp feh htop hwinfo pydf highlight atool libcaca rsync rtorrent extract dzen2 emacs mplayer ntfs-3g
+yaourt -S alsi aspell bc colordiff colorsvn cope-git dictd diction dmidecode dehtml elinks iftop lesspipe highlight mpd ncdu ncmpcpp feh htop hwinfo pydf highlight atool libcaca rsync rtorrent extract dzen2 emacs w3m emacs-w3m mplayer ntfs-3g

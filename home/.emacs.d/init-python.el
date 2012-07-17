734bd5d86ca27874727fdfcd4d37519c0767f10e
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-17 15:14:16 +1000

    emacs: fixed comments

--- home/.emacs.d/init-python.el
+++ home/.emacs.d/init-python.el
@@ -34,27 +34,21 @@
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pychecker-init)))
 
-;; pylookup
-;; https://github.com/tsgates/pylookup
 
+;; pylookup: https://github.com/tsgates/pylookup
 ;; add pylookup to your loadpath, ex) ~/.emacs.d/pylookup
 (setq pylookup-dir "~/.emacs.d/pylookup")
 (add-to-list 'load-path pylookup-dir)
-
 ;; load pylookup when compile time
 (eval-when-compile (require 'pylookup))
-
 ;; set executable file and db file
 (setq pylookup-program (concat pylookup-dir "/pylookup.py"))
 (setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
-
 ;; set search option if you want
 (setq pylookup-search-options '("--insensitive" "0" "--desc" "0"))
-
 ;; to speedup, just load it on demand
 (autoload 'pylookup-lookup "pylookup"
   "Lookup SEARCH-TERM in the Python HTML indexes." t)
-
 (autoload 'pylookup-update "pylookup"
   "Run pylookup-update and create the database at `pylookup-db-file'." t)
 

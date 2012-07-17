208baa04181f3ecdbbd2fec649dbb302f57bef34
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-17 11:33:24 +1000

    Renamed: install-11-ropemacs.sh -> install-11-python.sh

---
+++ install-11-python.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+yaourt -S python2-pip python2-ropemacs python2-pyflakes ipython2 ipython2-docs
+sudo pip2 install pep8
--- install-11-ropemacs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-yaourt -S python2-pip python2-ropemacs python2-pyflakes
-sudo pip2 install pep8

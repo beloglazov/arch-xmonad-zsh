f6dd552c5fa64f7d85c12983b0c8ccd97d56ec42
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-08-29 10:13:59 +1000

    Switched modMask from 3 to 4in xmonad, updated ~/.Xmodmap

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -23,7 +23,7 @@ myLayout = tiled ||| Full
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
-myModMask = mod3Mask
+myModMask = mod4Mask
 
 main = do
 	xmonad $ ewmh defaultConfig

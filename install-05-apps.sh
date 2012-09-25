99fe3434cff3445d799106589cea6005113d7b79
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-09-25 17:15:36 +1000

    Added glances

--- install-05-apps.sh
+++ install-05-apps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yaourt -S xorg xorg-xinit haskell-xmonad haskell-xmonad-contrib haskell-xmonad-extras consolekit rxvt-unicode dmenu numlockx xxkb chromium rsync jdk7-openjdk apache-ant maven lftp unzip tree subversion slock alsa-utils urxvt-perls-git flashplugin autojump openntpd ranger chromium-stable-libpdf ack
+yaourt -S xorg xorg-xinit haskell-xmonad haskell-xmonad-contrib haskell-xmonad-extras consolekit rxvt-unicode dmenu numlockx xxkb chromium rsync jdk7-openjdk apache-ant maven lftp unzip tree subversion slock alsa-utils urxvt-perls-git flashplugin autojump openntpd ranger chromium-stable-libpdf ack glances
 
 mkdir -p ~/.config/ranger
 ranger --copy-config=scope

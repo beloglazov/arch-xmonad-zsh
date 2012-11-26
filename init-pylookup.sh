eb14f9b3901e8787807a07330a69deb5e23df64c
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-11-26 12:53:37 +1100

    Merged init-pylookup into install-11-python

--- init-pylookup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd home/.emacs.d/modes/pylookup
-wget http://docs.python.org/archives/python-2.7.3-docs-html.zip
-unzip python-2.7.3-docs-html.zip
-rm python-2.7.3-docs-html.zip
-./pylookup.py -u python-2.7.3-docs-html
-cd -

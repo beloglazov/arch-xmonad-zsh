9c7a63e551a2033553b6218005d4f17764444659
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-06-06 16:53:27 +1000

    Removed cope-git

--- install-05-apps.sh
+++ install-05-apps.sh
@@ -2,7 +2,7 @@
 
 yaourt -S openssh chromium rsync jdk7-openjdk apache-ant maven lftp unzip tree \
 	subversion alsa-utils flashplugin ntp ranger chromium-stable-libpdf \
-	ack glances alsi aspell aspell-en ispell bc colordiff colorsvn cope-git dictd \
+	ack glances alsi aspell aspell-en ispell bc colordiff colorsvn dictd \
 	diction dmidecode elinks htop iftop lesspipe highlight mpd ncdu ncmpcpp feh \
 	hwinfo pydf atool libcaca rtorrent extract dzen2 emacs w3m emacs-w3m-cvs mplayer \
 	ntfs-3g cloc zathura zathura-pdf-poppler acroread dtrx dosfstools ntfsprogs \

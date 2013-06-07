7b50a00af22ff91aa1f026755b3600a288dc4d81
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-06-07 14:38:01 +1000

    Removed extract, added unrar

--- install-05-apps.sh
+++ install-05-apps.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-yaourt -S openssh chromium rsync jdk7-openjdk apache-ant maven lftp unzip tree \
+yaourt -S openssh chromium rsync jdk7-openjdk apache-ant maven lftp unzip unrar tree \
 	subversion alsa-utils flashplugin ntp ranger chromium-libpdf \
 	ack glances aspell aspell-en ispell bc colordiff colorsvn fasd \
 	dmidecode elinks htop iftop lesspipe highlight mpd ncdu ncmpcpp feh \
-	hwinfo pydf atool libcaca rtorrent extract dzen2 emacs mplayer tcl tk lnav \
+	hwinfo pydf atool libcaca rtorrent dzen2 emacs mplayer tcl tk lnav \
 	ntfs-3g cloc zathura zathura-pdf-poppler dtrx dosfstools ntfsprogs ctags gvim
 
 mkdir -p ~/.config/ranger

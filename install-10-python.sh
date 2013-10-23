b86541676b3ca63f8bdfcb5ae60bfee933a25921
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-10-23 17:39:53 +1100

    Added linking python2 to python

--- install-10-python.sh
+++ install-10-python.sh
@@ -3,3 +3,6 @@
 yaourt -S python2-pip python2-ropemacs python2-pyflakes ipython2 ipython2-docs \
     bpython2
 sudo pip2 install pep8
+sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python2 /usr/bin/python
+sudo ln -s /usr/bin/pip2 /usr/bin/pip

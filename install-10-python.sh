bfc41de35cb92f285c50bc562f83bf7fe632c378
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-12-02 04:01:48 +1100

    Removed ipython2-docs

--- install-10-python.sh
+++ install-10-python.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-yaourt -S python2-pip python2-ropemacs python2-pyflakes ipython2 ipython2-docs \
-    bpython2
+yaourt -S python2-pip python2-ropemacs python2-pyflakes ipython2 bpython2
 sudo pip2 install pep8
 sudo rm /usr/bin/python
 sudo ln -s /usr/bin/python2 /usr/bin/python

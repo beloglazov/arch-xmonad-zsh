6e79da7049b38dbecfdf16d1322386a06cd9aec8
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-01-30 12:10:45 +1100

    hawk: added to* functions

--- home/.hawk/prelude.hs
+++ home/.hawk/prelude.hs
@@ -2,3 +2,18 @@
 import Prelude
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.List as L
+
+to :: (Read a) => B.ByteString -> a
+to = read . B.unpack
+
+toBool :: B.ByteString -> Bool
+toBool = to
+
+toInt :: B.ByteString -> Int
+toInt = to
+
+toFloat :: B.ByteString -> Float
+toFloat = to
+
+toDouble :: B.ByteString -> Double
+toDouble = to

de8035643baec3ff7fe79965e5b764fdae323b97
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-04-04 12:20:06 +1100

    Added hlint

--- install-03-haskell-repo.sh
+++ install-03-haskell-repo.sh
@@ -2,5 +2,5 @@
 
 sudo sed -i 's/\[extra\]/\[haskell\]\nServer = http:\/\/xsounds.org\/~haskell\/core\/$arch\n\n\[extra\]/g' /etc/pacman.conf
 yaourt -Syu
-sudo pacman-key --lsign-key 4209170B 
+sudo pacman-key --lsign-key 4209170B
 yaourt -Syu
